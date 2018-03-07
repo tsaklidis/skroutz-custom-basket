966ccdbdb4cdec728cb2f6d63fdbb67fe1abb6e3
Stefanos I. Tsaklidis <ctefanos.t@gmail.com>
2018-03-07 23:13:12 +0200

    Allowedhosts

--- skroutzbasket/settings/base.py
+++ skroutzbasket/settings/base.py
@@ -21,7 +21,7 @@ SECRET_KEY = 'pasyj_x*1!^$u9%bp9!pwm#)z^(sf%w!%jsy4@7obzdf7@%__5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.gruber.ga', 'gruber.ga']
 
 
 # Application definition
