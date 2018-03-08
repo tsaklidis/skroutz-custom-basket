8c2f31f5422fe9c2231f5f78ca956fd2f5a8596b
Stefanos I. Tsaklidis <ctefanos.t@gmail.com>
2018-03-08 11:08:51 +0200

    example file

--- skroutzbasket/settings/__init__.py
+++ skroutzbasket/settings/__init__.py
@@ -3,6 +3,6 @@ from .base import *
 try:
     from .local import *
 except ImportError, exc:
-    exc.args = tuple(['%s (Create your local.py?)' %
+    exc.args = tuple(['%s (Create your own local.py)' %
                      exc.args[0]])
     raise exc
