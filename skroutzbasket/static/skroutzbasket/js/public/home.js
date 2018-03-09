98068902d492b48ef4f7c90ceb842e770eb45f5e
Stefanos I. Tsaklidis <ctefanos.t@gmail.com>
2018-03-09 17:51:22 +0200

    undefined js

--- skroutzbasket/static/skroutzbasket/js/public/home.js
+++ skroutzbasket/static/skroutzbasket/js/public/home.js
@@ -11,6 +11,10 @@ document.location.search.replace(/\??(?:([^=]+)=([^&]*)&?)/g, function () {
 
 
 $(document).ready(function() {
+	if ($_GET["token"] == "undefined" || $_GET["token"] == undefined)}{
+		$_GET["token"] = '';
+	}
+
 	if ($_GET["token"].length < 17) {
 		$('#link').attr('disabled', 'true', 'placeholder',"You can't add new items.");
 	}
