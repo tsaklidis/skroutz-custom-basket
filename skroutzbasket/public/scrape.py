d3ff367796037025c2d4847da5a0a9aee7c3aaac
Stefanos I. Tsaklidis <ctefanos.t@gmail.com>
2018-08-29 17:03:52 +0300

    Try free proxy

--- skroutzbasket/public/scrape.py
+++ skroutzbasket/public/scrape.py
@@ -25,7 +25,7 @@ def get_items(site):
 
         try:
 
-            proxy = urllib2.ProxyHandler({'http': '185.229.65.9:41258'})
+            proxy = urllib2.ProxyHandler({'http': '148.217.94.54:3128'})
             opener = urllib2.build_opener(proxy)
             urllib2.install_opener(opener)
 
